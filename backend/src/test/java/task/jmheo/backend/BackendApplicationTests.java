e9c99029ffb41aafb6d9b3dc06c911d60e2855c5
JaeMinHeo95 <rfvgbn21@naver.com>
2023-06-09 02:01:48 +0900

    초기설정

---
+++ backend/src/test/java/task/jmheo/backend/BackendApplicationTests.java
@@ -0,0 +1,13 @@
+package task.jmheo.backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
